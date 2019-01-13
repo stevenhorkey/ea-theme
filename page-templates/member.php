21a70acb08e56803b8a9f4e6d3ab4976622909fb
stevenhorkey <code@stevenhorkey.com>
2019-01-12 18:50:59 -0700

    member page

---
+++ page-templates/member.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template Name: Member Page Template
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+get_hero();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+// echo get_post_field('post_content', $post->ID);
+
+?>
+
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<main class="site-main w-100 text-center" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #page-wrapper -->
+
+<?php get_footer(); ?>
