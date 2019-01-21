dc69a08638c031401e253633a2a2af279a1c3bb5
stevenhorkey <code@stevenhorkey.com>
2019-01-21 07:42:36 -0700

    out of mantenance mode

--- functions.php
+++ functions.php
@@ -53,12 +53,12 @@ function is_last_page() {
 }
 
 // Activate WordPress Maintenance Mode
-function wp_maintenance_mode(){
-    if(!current_user_can('edit_themes') && !is_user_logged_in()){
-        wp_die('<h1 style="color:#88a4ac">Website Under Maintenance</h1><br />We are performing scheduled maintenance. We will be back on-line shortly!');
-    }
-}
-add_action('get_header', 'wp_maintenance_mode');
+// function wp_maintenance_mode(){
+//     if(!current_user_can('edit_themes') && !is_user_logged_in()){
+//         wp_die('<h1 style="color:#88a4ac">Website Under Maintenance</h1><br />We are performing scheduled maintenance. We will be back on-line shortly!');
+//     }
+// }
+// add_action('get_header', 'wp_maintenance_mode');
 
 function addPostFormHTMLInstance(){
 	// Insert post form instance into db.
